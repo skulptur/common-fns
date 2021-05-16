c2b49431b9c32bf893f90dcb55e12bd51854f514
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-05-15 22:32:06 -0300

    remove times

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export { always } from './lib/always'
+export { identity } from './lib/identity'
 export { isEqual } from './lib/isEqual'
 export { noop } from './lib/noop'
-export { times } from './lib/times'
--- src/lib/times.ts
+++
@@ -1,3 +0,0 @@
-export const times = <T>(iterations: number, callback: (index: number) => T): Array<T> => {
-  return [...new Array(iterations)].map((_, index) => callback(index))
-}
